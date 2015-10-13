42d1f1589f6055b9baba6c68899039a54a3712d7
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-13 19:31:13 -0400

    Fixed merge conflict?

--- lib/githubber.rb
+++ lib/githubber.rb
@@ -2,21 +2,20 @@ require "httparty"
 require "pry"
 
 require "githubber/version"
-<<<<<<< HEAD
+
 require "githubber/issues"
 
 # require "githubber/pull_requests"
 
 
-=======
+
 require "githubber/pull_requests"
->>>>>>> master
+
 
 module Githubber
   # Your code goes here...
   binding.pry
 end
-<<<<<<< HEAD
 #binding.pry
 
 
@@ -25,7 +24,6 @@ end
 # api = Issues.new("7df3a5d15fa6a42fef0bb325328cfc773f408dde")
 # binding.pry
 # api.make_comment("rmcwilliam", "githubber", 2,"blah blah")
-=======
 
 
->>>>>>> master
+
