beee06a77d3bb90fb7ca7b0814172a1119a8ea9a
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-13 17:21:50 -0400

    updated issues class

--- lib/githubber/issues.rb
+++ lib/githubber/issues.rb
@@ -10,12 +10,36 @@ module Githubber
       }
     end
 
-    def list_pulls(owner, repo)
-      PullRequests.get("/repos/#{owner}/#{repo}/pulls", :headers => @auth)
+# POST /repos/:owner/:repo/issues/:number/comments :body => options.to_json
+# https://github.com/rmcwilliam/githubber/issues/2
+
+    def make_comment(owner, repo, number, comment)
+      Issues.post("/repos/#{owner}/#{repo}/issues/#{number}/comments",:headers => @auth,:body => {"body" => comment}.to_json) ## needs auth!!! Thats why wasn't working!
     end
 
-    def update_user(options={})
-      PullRequests.patch("/user", :headers => @auth, :body => options.to_json)
+    # GET /repos/:owner/:repo/issues
+    def list_issues(owner, repo)
+      Issues.get("/repos/#{owner}/#{repo}/issues", :headers => @auth) # authentication up's rate/limit here but not necessary
     end
-  end
+   
+    #https://github.com/rmcwilliam/githubber/issues/2   
+    # PATCH /repos/:owner/:repo/issues/:number
+    def close_issue(owner, repo, number)
+      Issues.patch("/repos/#{owner}/#{repo}/issues/#{number}", :headers => @auth) #needs auth
+    end
+  end 
 end
+
+
+# api = Issues.new("7df3a5d15fa6a42fef0bb325328cfc773f408dde")
+# binding.pry
+# api.make_comment("rmcwilliam", "githubber", 2,"blah blah")
+
+
+#comment on an issue
+#list issues for a repo 
+#close an issue
+
+# def update_user(options={})
+#       PullRequests.patch("/user", :headers => @auth, :body => options.to_json)
+#     end
