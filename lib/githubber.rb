c0b0618f2036bc431ecb7cffad3bac2843a6b9be
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-23 15:09:45 -0500

    cleaning up

--- lib/githubber.rb
+++ lib/githubber.rb
@@ -1,23 +1,19 @@
 require "httparty"
 require "pry"
 
+
 require "githubber/api_calls"
 require "githubber/version"
 require "githubber/issues"
+require "githubber/hard_mode"
 require "githubber/pull_requests"
 
 
 module Githubber
-  # Your code goes here...
-  binding.pry
+
 end
-#binding.pry
-
-
-
 
 # api = Issues.new(access token)
-# binding.pry
 # api.close_issue("rmcwilliam", "githubber", 2, "state":"closed")
 # api.make_comment("rmcwilliam", "githubber", 1,"blah blah")
 
--- lib/githubber/issues.rb
+++ lib/githubber/issues.rb
@@ -10,36 +10,16 @@ module Githubber
       }
     end
 
-# POST /repos/:owner/:repo/issues/:number/comments 
-# https://github.com/rmcwilliam/githubber/issues/2
-
     def make_comment(owner, repo, number, comment)
-      Issues.post("/repos/#{owner}/#{repo}/issues/#{number}/comments",:headers => @auth,:body => {"body" => comment}.to_json) ## needs auth!!! Thats why wasn't working!
+      Issues.post("/repos/#{owner}/#{repo}/issues/#{number}/comments",:headers => @auth,:body => {"body" => comment}.to_json) 
     end   
 
-    # GET /repos/:owner/:repo/issues
     def list_issues(owner, repo)
-      Issues.get("/repos/#{owner}/#{repo}/issues", :headers => @auth) # authentication up's rate/limit here but not necessary
-    end   # could set the reulsts of this equal to a variable. results = api.list_issues()
-   
-    #https://github.com/rmcwilliam/githubber/issues/2   
-    # PATCH /repos/:owner/:repo/issues/:number                # api.close_issue("rmcwilliam", "githubber", 1, "state":"closed")
+      Issues.get("/repos/#{owner}/#{repo}/issues", :headers => @auth) 
+    end   
+                
     def close_issue(owner, repo, number, options={})
-      Issues.patch("/repos/#{owner}/#{repo}/issues/#{number}",:headers => @auth, :body => options.to_json) #needs auth
+      Issues.patch("/repos/#{owner}/#{repo}/issues/#{number}",:headers => @auth, :body => options.to_json) 
     end
   end 
 end
-
-
-# api = Issues.new(access token)
-# binding.pry
-# api.make_comment("rmcwilliam", "githubber", 2,"blah blah")
-
-
-#comment on an issue
-#list issues for a repo 
-#close an issue
-
-# def update_user(options={})
-#       PullRequests.patch("/user", :headers => @auth, :body => options.to_json)
-#     end
